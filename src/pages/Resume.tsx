e91da8937eb3ca97bf20726e8fbe1f108cf0a696
kingOfSchottland <46346267+ssKimholy@users.noreply.github.com>
2023-06-06 20:08:24 +0900

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,28 +2,25 @@ const Resume = () => {
   return (
     <div>
       <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
-      <p>2022.08 ~ 2022.11</p>
+      <h5>2022.08 ~ 2022.11</h5>
       <div style={{ marginTop: 10 }}></div>
-      <p>
+      <h5>
         인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
         투고했습니다.
-      </p>
+      </h5>
 
-      <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
-      <p>2022.08 ~ 2022.11</p>
+      <h2 style={{ marginTop: 10 }}>Project fair DevDay 동상</h2>
+      <h5>2022.11</h5>
       <div style={{ marginTop: 10 }}></div>
-      <p>
-        인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
-        투고했습니다.
-      </p>
+      <h5>충남대 컴퓨터융합학부 행사 project fair devday라는 코딩테스트 대회에서 동상 수상했습니다.</h5>
 
-      <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
-      <p>2022.08 ~ 2022.11</p>
+      <h2 style={{ marginTop: 10 }}>CNU Library Wi-Fi Analyzer(WIA)</h2>
+      <p>2023.04 ~ 2023.06</p>
       <div style={{ marginTop: 10 }}></div>
-      <p>
-        인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
-        투고했습니다.
-      </p>
+      <h5>
+        react를 사용해 실시간 충남대 중앙도서관 wifi 분석기를 개발했습니다. FE 개발을 맡았고, 실시간 도서관 wifi 사용자 수를 가져와 혼잡도를
+        사용자에게 보여주는 서비스입니다.
+      </h5>
     </div>
   );
 };
