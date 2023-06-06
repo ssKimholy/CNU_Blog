f94d243c2a96b9840e2b31147096fe5257744bd7
kingOfSchottland <46346267+ssKimholy@users.noreply.github.com>
2023-06-06 20:00:36 +0900

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,31 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
+      <p>2022.08 ~ 2022.11</p>
+      <div style={{ marginTop: 10 }}></div>
+      <p>
+        인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
+        투고했습니다.
+      </p>
+
+      <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
+      <p>2022.08 ~ 2022.11</p>
+      <div style={{ marginTop: 10 }}></div>
+      <p>
+        인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
+        투고했습니다.
+      </p>
+
+      <h2 style={{ marginTop: 10 }}>Driving Motion System</h2>
+      <p>2022.08 ~ 2022.11</p>
+      <div style={{ marginTop: 10 }}></div>
+      <p>
+        인지부하 기반 운전자 상태 인식 및 영어 학습 서비스 개발입니다. react를 사용해 FE개발을 맡았고, 실험에도 활용해 한국 hci 학회에 논문을
+        투고했습니다.
+      </p>
+    </div>
+  );
 };
 
 export default Resume;
